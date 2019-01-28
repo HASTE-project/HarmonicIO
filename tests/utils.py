68cb768c7ec29a91c471d9ba39690bdb9eac9126
Ben Blamey <blamey.ben@gmail.com>
2019-01-28 18:49:29 +0100

    tweak tests

--- tests/utils.py
+++ tests/utils.py
@@ -13,15 +13,18 @@ def run_module(module_name, timeout_seconds=10):
     Run a python3 module in a sub-process, and block until completion/timeout.
     '''
     print('cwd: ' + os.getcwd())
+    print('executable: ' + sys.executable )
+    print('base prefix: ' + sys.base_prefix )
+    # print('executable: ' + sys.real_prefix )
 
-    result = subprocess.run(['python',
+    result = subprocess.run([sys.executable,
                              '-m',
                              module_name],
                             # capture_output=True,
                             # text=True,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
-
+                            universal_newlines=True,
                             timeout=timeout_seconds,
                             cwd='test_root')
     return result
